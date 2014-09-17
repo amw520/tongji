eb57931afacd048226cd108cf95f8dfa5f744b48
amw520 <a215974929@qq.com>
2014-09-17 10:41:27 +0800

    添加provider

--- provider.php
+++ provider.php
@@ -15,8 +15,8 @@
   $orderdate=$_GET["orderdate"];
   $gamename=$_GET["gamename"];
   $act=$_GET["act"];
-	$sql="INSERT INTO `rechargeinfo` (`id` ,`ipadcoed` ,`usrid` ,`gameid` ,`custername` ,`chargetime` ,`chargeday` ,`chargecontent`) VALUES (NULL , '01', ".$orderusr.", ".$gamename.", ".$curstername.",  '".$ordertime."', '".$orderdate."', ".$chargetype.")";
-$sqlq="SELECT count(*),sum(chargecontent) FROM `rechargeinfo` WHERE 1";
+	$sql="INSERT INTO 'rechargeinfo' ('id' ,'ipadcoed' ,'usrid' ,'gameid' ,'custername' ,'chargetime' ,'chargeday' ,'chargecontent') VALUES (NULL , '01', ".$orderusr.", ".$gamename.", ".$curstername.",  '".$ordertime."', '".$orderdate."', ".$chargetype.")";
+$sqlq="SELECT count(*),sum(chargecontent) FROM 'rechargeinfo' WHERE 1";
 if($act="query"){
 	if($curstername!="undefined"){
 	$sqlq=$sqlq." and custername=".$curstername;
@@ -45,10 +45,93 @@ echo json_encode($resstr);
  * @access public
  *
  *************************************************************/
- function addChargeItem($orderusr,$gamename){
-	$sql="INSERT INTO `rechargeinfo` (`id` ,`ipadcoed` ,`usrid` ,`gameid` ,`custername` ,`chargetime` ,`chargeday` ,`chargecontent`) VALUES (NULL , '01', ".$orderusr.", ".$gamename.", ".$curstername.",  '".$ordertime."', '".$orderdate."', ".$chargetype.")";
+ function addChargeItem($ipadcode,$orderusr,$gamename,$curstername,$ordertime,$orderdate,$chargetype){
+	$sql="INSERT INTO 'rechargeinfo' ('id' ,'ipadcoed' ,'usrid' ,'gameid' ,'custername' ,'chargetime' ,'chargeday' ,'chargecontent') VALUES (NULL , ".$ipadcode", ".$orderusr.", ".$gamename.", ".$curstername.",  '".$ordertime."', '".$orderdate."', ".$chargetype.")";
 	$result=mysql_query($sql);
 	echo $result;
  }
- 
+ /**************************************************************
+ *
+ * 查询总额
+ * @param 
+ * @return string  
+ * @access public
+ *
+ *************************************************************/
+ function queryTotalMount($curstername,$chargetype,$orderusr,$gamename,$date0,$date1,$time0,$time1){
+	$sqlq="SELECT COUNT(*),SUM(chargecontent),sum(nanfei) from rechargeinfo as a INNER JOIN chargeamount as b on a.chargecontent=b.'value' where 1";
+	if($curstername!="undefined"){
+	$sqlq=$sqlq." and custername=".$curstername;
+	}
+	if($chargetype!="undefined"){
+	$sqlq=$sqlq." and chargecontent=".$chargetype;
+	}
+	if($orderusr!="undefined"){
+	$sqlq=$sqlq." and usrid=".$orderusr;
+	}
+
+	if($gamename!="undefined"){
+	$sqlq=$sqlq." and gameid=".$gamename;
+	}
+	if($date0!="undefined"){
+		if($date1!="undefined"){
+			
+		}else{
+		$sqlq=$sqlq." and chargeday=".$date0;
+		}
+	}
+	if($time0!="undefined"){
+		if($time1!="undefined"){
+			
+		}else{
+		$sqlq=$sqlq." and chargetime=".$time0;
+		}
+	}
+
+	$result=mysql_query($sqlq);
+	$resstr=mysql_fetch_array($result);
+	echo json_encode($resstr);
+ }
+  /**************************************************************
+ *
+ * 添加用户
+ * @param 
+ * @return string  
+ * @access public
+ *
+ *************************************************************/
+  function addUsr($usrname){
+	$sql="INSERT INTO usr(name) VALUES('".$usrname."')";
+	$result=mysql_query($sql);
+	
+	echo json_encode($result);
+ }
+   /**************************************************************
+ *
+ * 添加面值
+ * @param 
+ * @return string  
+ * @access public
+ *
+ *************************************************************/
+  function addFaceValue($typename,$value,$nanfei){
+	$sql="INSERT INTO chargeamount(typename,value,nanfei) VALUES('".$typename."',".$value.",".$nanfei.")";
+	$result=mysql_query($sql);
+	
+	echo json_encode($result);
+ }
+    /**************************************************************
+ *
+ * 添加新游戏
+ * @param 
+ * @return string  
+ * @access public
+ *
+ *************************************************************/
+  function addGame($gamename){
+	$sql="INSERT INTO gamename(gamename) VALUES('".$gamename."')";
+	$result=mysql_query($sql);
+	
+	echo json_encode($result);
+ }
 ?>
